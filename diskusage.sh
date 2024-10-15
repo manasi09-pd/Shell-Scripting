a35dc7d5b25aaaad5cccd151948c2230a3b10f27
Manasi <72643566+manasi09-pd@users.noreply.github.com>
2024-10-15 23:55:19 +0530

    Update the threshold

--- diskusage.sh
+++ diskusage.sh
@@ -12,7 +12,7 @@ disk_usage=`df -kh | grep /dev/root | awk '{print $5}' | cut -d '%' -f 1`
 
 if [ "$disk_usage" -ge "$THRESHOLD" ];
 then
-	echo "Alert: The disk usage on the machine is $disk_usage%  where the it exceeds the threshold value $THRESHOLD%"
+	echo "Alert: The disk usage on the machine is $disk_usage%  where the it exceeds the threshold $THRESHOLD%"
 else
 	echo "Disk usage is $disk_usage which is under the limit"
 fi
